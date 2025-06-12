45dea55528c368c091285bb1bea5106ff5c1449b
samurai-ctrl <davidlnava01@gmail.com>
2025-06-12 14:09:35 +0200

    Kit Digital Ikonox

---
+++ src/main/java/com/cursoceat/kitdigital/KitDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.cursoceat.kitdigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitDigitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitDigitalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cursoceat/kitdigital/KitDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoceat.kitdigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitDigitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
